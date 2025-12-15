74742d8eda260e468c6da03952d8b5f91241ad94
Galin <sao4449@abv.bg>
2025-12-15 14:27:38 +0200

    Add header and search bar

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,22 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+
 import './App.css'
+import Search from "./components/Search.jsx";
+import {useState} from "react";
 
 function App() {
+    const [searchTerm, setSearchTerm] = useState('')
 
   return (
-    <>
-        <h3 className="text-3xl">Hello</h3>
-    </>
+    <main>
+        <div className="pattern" />
+        <div className="wrapper">
+            <header>
+                <img src='../public/hero-img.png' alt='hero' />
+                <h1>Find <span className="text-gradient">movies</span> you'll enjoy fast and easy</h1>
+            </header>
+            <Search searchTerm={searchTerm} setSearchTerm={setSearchTerm}  />
+        </div>
+    </main>
   )
 }
 
